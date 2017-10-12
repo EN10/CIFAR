05c0588d131f06db16a0f13334f1ddc6485e1f8d
Enio Fernandes <ef@cic.ac>
2017-10-12 12:38:29 +0000

    array

---
+++ cifar_array.py
@@ -0,0 +1,16 @@
+import cPickle
+import numpy as np
+
+path = 'cifar-10-batches-py/'
+db1 = 'data_batch_1'
+
+f = open(path+db1, 'rb')
+dict = cPickle.load(f)
+images = dict['data']
+#images = np.reshape(images, (10000, 3, 32, 32))
+labels = dict['labels']
+imagearray = np.array(images)
+labelarray = np.array(labels)
+    
+print imagearray.shape
+print labelarray.shape

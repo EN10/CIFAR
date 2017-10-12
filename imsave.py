dd7a6fbe74e574f67ac868e41e17201f5b866319
Enio Fernandes <ef@cic.ac>
2017-10-12 13:20:32 +0000

    imsave

--- imsave.py
+++ imsave.py
@@ -8,10 +8,6 @@ f = open(path+db1, 'rb')
 dict = cPickle.load(f)
 images = dict['data']
 images = np.reshape(images, (10000, 3, 32, 32))
-imagearray = np.array(images)
-
-i = 32
-img = np.dstack((images[i][0], images[i][1], images[i][2]))
 
 from scipy.misc import imsave
-imsave('image.jpg', img)
+imsave('image.jpg', images[46])     # image no 46

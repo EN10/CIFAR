3a9a61273fa9e64dc482ea679295fc615040a280
Enio Fernandes <ef@cic.ac>
2017-10-15 09:32:39 +0000

    create_datasets

--- cats.py
+++ cats.py
@@ -1,51 +1,11 @@
 from __future__ import division
-from load_cifar import load_batch
+from load_cifar import *
 import numpy as np
-import random
 
 imagearray, labelarray = load_batch()
 #print imagearray.shape          #   (10000, 3072)   3072 = 3, 32, 32
 #print labelarray.shape          #   (10000,)
-train_set_x = np.empty((200,3072))
-train_set_y = np.empty((1,200),dtype=np.int16)
-
-i = 0
-j = 0
-while (j < 200):                #   200 train images
-    x = random.randint(0,1)
-    if (labelarray[i] == 3):    #   Cats
-        train_set_x[j] = imagearray[i]
-        train_set_y[0,j] = 1
-        j+=1
-    elif (x % 2 == 0 and labelarray[i] != 3):   #    NOT Cats
-        train_set_x[j] = imagearray[i]
-        train_set_y[0,j] = 0
-        j+=1
-    i+=1
-    
-train_set_x = train_set_x.T     #   Reshape to (3072, 200) 
-
-test_set_x = np.empty((50,3072))
-test_set_y = np.empty((1,50),dtype=np.int16)
-
-i = 0
-j = 0
-while (j < 50):
-    x = random.randint(0,1)
-    if (labelarray[9999-i] == 3):
-        test_set_x[j] = imagearray[9999-i]
-        test_set_y[0,j] = 1
-        j+=1
-    elif (x % 2 == 0 and labelarray[i] != 3):
-        test_set_x[j] = imagearray[9999-i]
-        test_set_y[0,j] = 0
-        j+=1
-    i+=1
-
-test_set_x = test_set_x.T       #   Reshape to (3072, 50)
-
-train_set_x = train_set_x/255.  # 0-255 -> 0-1
-test_set_x = test_set_x/255.
+train_set_x, train_set_y, test_set_x, test_set_y = create_datasets(imagearray, labelarray)
 
 def sigmoid(z):
 
--- load_cifar.py
+++ load_cifar.py
@@ -1,5 +1,6 @@
 import cPickle
 import numpy as np
+import random
 
 def load_batch():
     path = 'cifar-10-batches-py/'
@@ -14,5 +15,49 @@ def load_batch():
     labelarray = np.array(labels)   #   (10000,)
     
     return imagearray, labelarray
+
+def create_datasets(imagearray, labelarray):
+    train_set_x = np.empty((200,3072))
+    train_set_y = np.empty((1,200),dtype=np.int16)
+
+    i = 0
+    j = 0
+    while (j < 200):                #   200 train images
+        x = random.randint(0,1)
+        if (labelarray[i] == 3):    #   Cats
+            train_set_x[j] = imagearray[i]
+            train_set_y[0,j] = 1
+            j+=1
+        elif (x % 2 == 0 and labelarray[i] != 3):   #    NOT Cats
+            train_set_x[j] = imagearray[i]
+            train_set_y[0,j] = 0
+            j+=1
+        i+=1
+        
+    train_set_x = train_set_x.T     #   Reshape to (3072, 200) 
+    
+    test_set_x = np.empty((50,3072))
+    test_set_y = np.empty((1,50),dtype=np.int16)
+
+    i = 0
+    j = 0
+    while (j < 50):
+        x = random.randint(0,1)
+        if (labelarray[9999-i] == 3):
+            test_set_x[j] = imagearray[9999-i]
+            test_set_y[0,j] = 1
+            j+=1
+        elif (x % 2 == 0 and labelarray[i] != 3):
+            test_set_x[j] = imagearray[9999-i]
+            test_set_y[0,j] = 0
+            j+=1
+        i+=1
+
+    test_set_x = test_set_x.T       #   Reshape to (3072, 50)
+
+    train_set_x = train_set_x/255.  # 0-255 -> 0-1
+    test_set_x = test_set_x/255.
+
+    return train_set_x, train_set_y, test_set_x, test_set_y
 
 #load()

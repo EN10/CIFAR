181b32fd72181a3ee17fa80198f6f171431e032a
Enio Fernandes <ef@cic.ac>
2017-11-07 09:25:48 +0000

    parameters

--- tf.py
+++ tf.py
@@ -3,23 +3,9 @@ import tensorflow as tf
 
 def predict(X, parameters):
     
-    W1 = tf.convert_to_tensor(parameters["W1"])
-    b1 = tf.convert_to_tensor(parameters["b1"])
-    W2 = tf.convert_to_tensor(parameters["W2"])
-    b2 = tf.convert_to_tensor(parameters["b2"])
-    W3 = tf.convert_to_tensor(parameters["W3"])
-    b3 = tf.convert_to_tensor(parameters["b3"])
-    
-    params = {"W1": W1,
-              "b1": b1,
-              "W2": W2,
-              "b2": b2,
-              "W3": W3,
-              "b3": b3}
-    
     x = tf.placeholder(tf.float32, [X.shape[0], None])
     
-    z3 = forward_propagation_for_predict(x, params)
+    z3 = forward_propagation_for_predict(x, parameters)
     p = tf.argmax(z3)
     
     sess = tf.Session()
